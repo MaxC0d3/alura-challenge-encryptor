a1ca4d4cd6e6142317a2f5adb31eb343edc4f9dc
MaxC0d3 <maximitianoreste@gmail.com>
2023-04-20 16:15:13 -0500

    app initializated

---
+++ main.js
@@ -0,0 +1,43 @@
+const $ = (s) => document.querySelector(s);
+
+const $ta = $("#text-content-area");
+const $e = $("#encrypt");
+const $d = $("#decrypt");
+const $r = $("#result");
+const $f = $("form");
+
+let value;
+
+const keys = {
+  e: "enter",
+  i: "imes",
+  a: "ai",
+  o: "ober",
+  u: "ufat"
+}
+
+$f.addEventListener("submit", (e) => e.preventDefault());
+
+function encrypt(t) {
+  return t.replace(/[eiou]/g, (c) => keys[c]);
+}
+
+function decrypt(t) {
+  return t.replace(/(enter|imes|ai|ober|ufat)/g, (c) => {
+    for (let k in keys) {
+      if (keys[k] === c) return k;
+    }
+  });
+}
+
+$ta.addEventListener("input", (e) => {
+  value = e.target.value;
+});
+
+$e.addEventListener("click", () => {
+  $r.innerText = encrypt(value);
+});
+
+$d.addEventListener("click", () => {
+  $r.innerText = decrypt(value);
+});
